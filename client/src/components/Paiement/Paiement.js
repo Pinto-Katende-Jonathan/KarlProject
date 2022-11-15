a6b09f9bbd0058e2e31d68b91515509d3d6178d0
Jonathan Katende Pinto <79837754+Pinto-Katende-Jonathan@users.noreply.github.com>
2022-11-15 16:39:21 +0200

    Delete client directory

--- client/src/components/Paiement/Paiement.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Paiement() {
-  return (
-    <div>Paiement</div>
-  )
-}
-
-export default Paiement
--- client/src/components/Prestation/Prestation.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Prestation() {
-  return (
-    <div>Prestation</div>
-  )
-}
-
-export default Prestation
