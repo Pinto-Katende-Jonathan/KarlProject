a8c616f3ec065c58234d3466d1817f45bb09b253
Jonathan Katende Pinto <79837754+Pinto-Katende-Jonathan@users.noreply.github.com>
2022-11-15 09:32:17 +0200

    Delete src directory

--- src/components/Cours/Cours.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Cours() {
-  return (
-    <div>Cours</div>
-  )
-}
-
-export default Cours
--- src/components/Home.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Home() {
-  return (
-    <div>Welcome to the home page</div>
-  )
-}
-
-export default Home
