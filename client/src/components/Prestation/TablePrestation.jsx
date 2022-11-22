5a3ac51c69f64cd1c437abeb377db4e6c7e8936d
Jonathan Katende Pinto <79837754+Pinto-Katende-Jonathan@users.noreply.github.com>
2022-11-22 16:46:52 +0200

    Add files via upload

--- client/src/components/Prestation/TablePrestation.jsx
+++ client/src/components/Prestation/TablePrestation.jsx
@@ -6,6 +6,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import styles from './Prestation.module.css'
 
 function createData(name, calories, fat, carbs, protein) {
   return { name, calories, fat, carbs, protein };
@@ -21,6 +22,7 @@ const rows = [
 
 export default function BasicTable() {
   return (
+    <div className={styles.tablePrestation}>
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650}} aria-label="simple table">
         <TableHead>
@@ -50,5 +52,6 @@ export default function BasicTable() {
         </TableBody>
       </Table>
     </TableContainer>
+    </div>
   );
 }
